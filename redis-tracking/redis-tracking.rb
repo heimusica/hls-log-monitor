5356e20042cc05c5f2abedf5c7bf115c03b2ae28
Matias Insaurralde <matias@heinetwork.com>
2014-10-01 03:25:13 +0000

    some constants etc

--- redis-tracking/redis-tracking.rb
+++ redis-tracking/redis-tracking.rb
@@ -6,6 +6,7 @@ require 'digest/md5'
 require 'redis'
 require 'json'
 
+ACCESS_LOG, STREAM_PATH = '/var/log/nginx/access_log', '/hls/livestream'
 REDIS_PREFIX = 'hls.'
 REDIS_EXPIRE = 5
 
@@ -13,11 +14,11 @@ $redis = Redis.new()
 
 module RedisTracking
   def self.start_monitoring()
-    HLSLogMonitor( :access_log => '/var/log/nginx/access_log',
-                   :stream_path => '/hls/livestream'  ) do |entry|
+    HLSLogMonitor( :access_log => ACCESS_LOG,
+                   :stream_path => STREAM_PATH  ) do |entry|
       whitespace_splits, quote_splits = entry.split(' '), entry.chomp.split('"')
       ip_addr, user_agent = whitespace_splits.first, quote_splits.last
-      client_id = Digest::MD5.hexdigest( [ip_addr, user_agent].join() )
+      client_id = Digest::MD5.hexdigest( [ip_addr, user_agent].join() )[0,10]
       p [client_id, ip_addr, user_agent]
       self.set_key( client_id, { 'ip_addr' => ip_addr, 'user_agent' => user_agent } )
     end
@@ -33,6 +34,7 @@ module RedisTracking
     clients = {}
     $redis.keys(REDIS_PREFIX+"*").each do |k|
       raw_json = $redis.get(k)
+      k.gsub!(REDIS_PREFIX, '')
       clients.store(k, JSON.load(raw_json))
     end
     clients
